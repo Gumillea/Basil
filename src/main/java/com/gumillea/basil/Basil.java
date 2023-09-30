8934a1df6809d46d414eacd8f2364fdc82f59717
Gumillea <ea2171785569@outlook.com>
2023-09-30 18:15:42 +0800

    update textures

--- src/main/java/com/gumillea/basil/Basil.java
+++ src/main/java/com/gumillea/basil/Basil.java
@@ -7,26 +7,14 @@ import com.gumillea.basil.core.reg.data.BasilCompostableItems;
 import com.gumillea.basil.core.reg.data.tags.providers.BasilBlockTagsProvider;
 import com.gumillea.basil.core.reg.data.tags.providers.BasilItemTagsProvider;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 @Mod(Basil.MODID)
